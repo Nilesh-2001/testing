ca0652bd6612a724f8996487ac2e56b058cf1547
Nilesh kumar Verma <1941012123.e.nileshkumarverma@gmail.com>
2024-03-15 13:00:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
+//import Alert from "./components/Alert";
 
-function App(){
-  return <div><Message /></div>
+function App() {
+  let items = ["Giridih", "Dhanbad", "Wasseypur", "Jamshedpur", "Ranchi"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />  
+    </div>
+  );
+  
+
 }
+//function App(){
+  
+//}
 export default App;
